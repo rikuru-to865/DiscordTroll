a4206d636d00947ba4345ed568c730f4e33b6630
rikuru_to <usaginosipppo865@gmail.com>
2021-02-18 17:48:54 +0900

    first commit

---
+++ DiscordOxO/ChooseSpam.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+using System.Drawing;
+
+namespace DiscordOxO
+{
+    public enum Chooses { SpamServer = 1, SpamDM, AddToken, GenerateToken, NoChoose }
+
+
+    class ChooseSpam
+    {
+        public static Action Choose()
+        {
+        restart:
+            Action choose;
+
+
+            Console.ForegroundColor = Color.White;
+            Console.WriteLine("1.サーバーにスパム");
+            Console.WriteLine("2.DMにスパム");
+            Console.WriteLine("3.tokenを追加");
+            Console.WriteLine("4.tokenを生成");
+            int Choose = int.Parse(Console.ReadLine());
+            switch (Choose)
+            {
+                case 1:
+                    Console.WriteLine("1.サーバーにスパムに決定しました");
+                    return choose = AddToken.TokenAdder;
+                case 2:
+                    Console.WriteLine("2.DMにスパムに決定しました");
+                    return choose = SpamDM.DMSpamer;
+                case 3:
+                    Console.WriteLine("3.tokenを追加に決定しました");
+                    return choose = AddToken.TokenAdder;
+                case 4:
+                    Console.WriteLine("4.tokenを生成に決定しました");
+                    return choose = GenerateToken.TokenGenerrator;
+            }
+            Console.WriteLine("有効な数字を入力してください");
+            goto restart;
+        }
+    }
+}
---
+++ DiscordOxO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace DiscordOxO
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteAscii("DISCORD TROLL TOOL -V 0.1", Color.FromArgb(100, 100, 200));
+            Console.WriteLine("rikuru_to#7754", Color.Aqua);
+
+
+            Action UserChoose = ChooseSpam.Choose(); //選択肢を入力
+            UserChoose();
+        }
+    }
+}
